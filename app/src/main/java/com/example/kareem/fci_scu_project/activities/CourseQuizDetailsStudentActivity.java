1d69c986f7a7398a2e35298571e1bd35133435b8
kareem shata <37172672+kareemshatta@users.noreply.github.com>
2019-07-02 20:46:24 +0200

    download tutorial

--- app/src/main/java/com/example/kareem/fci_scu_project/activities/CourseQuizDetailsStudentActivity.java
+++ app/src/main/java/com/example/kareem/fci_scu_project/activities/CourseQuizDetailsStudentActivity.java
@@ -1,8 +1,11 @@
 package com.example.kareem.fci_scu_project.activities;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +27,8 @@ public class CourseQuizDetailsStudentActivity extends AppCompatActivity {
     private TextView solutionName;
     private TextView quizDeadline;
     private String solName;
+
+    private DownloadManager downloadManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,16 @@ public class CourseQuizDetailsStudentActivity extends AppCompatActivity {
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // download task here
+
+                final String taskName = TASK_DATA.getTaskName();
+
+                downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("https://matehub.azurewebsites.net/Data/Tasks/" + taskName+".pdf");
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setVisibleInDownloadsUi(true);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, uri.getLastPathSegment());
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                long reference = downloadManager.enqueue(request);
             }
         });
         sendButton.setOnClickListener(new View.OnClickListener() {
