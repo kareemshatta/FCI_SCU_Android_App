1b3254cbb803af4440d9bfe057988fe9ecb07799
youssefseddik <youssef.seddik.s@gmail.com>
2019-07-05 22:42:03 +0200

    modify home

---
+++ app/src/main/java/com/example/kareem/fci_scu_project/activities/SelectStudentActivity.java
@@ -0,0 +1,15 @@
+package com.example.kareem.fci_scu_project.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kareem.fci_scu_project.R;
+
+public class SelectStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_student);
+    }
+}
